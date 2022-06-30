0a1f4bc5af2b91658491595290d1f5ac51043123
tapupadhi <rashmiranjanpadhi19@gmail.com>
2022-06-30 22:21:35 +0530

    Added model for expenses. Added tests for it.

--- restapi/tests.py
+++ restapi/tests.py
@@ -1,12 +1,19 @@
-# from django.test import TestCase
-from unittest import TestCase
+from django.test import TestCase
+from restapi import models
 
 
 # Create your tests here.
-def two_integers_sum(a, b):
-    return a + b
+class TestModels(TestCase):
+    def test_expense(self):
+        expense = models.Expense.objects.create(
+            amount=249.99,
+            merchant="amazon",
+            description="anc headphones",
+            category="music",
+        )
+        inserted_expense = models.Expense.objects.get(pk=expense.id)
 
-
-class TestSum(TestCase):
-    def test_sum(self):
-        self.assertEqual(two_integers_sum(2, 1), 3)
+        self.assertEqual(249.99, inserted_expense.amount)
+        self.assertEqual("amazon", inserted_expense.merchant)
+        self.assertEqual("anc headphones", inserted_expense.description)
+        self.assertEqual("music", inserted_expense.category)
